5bd1b7d575d8a2f3b4eb1c947dee462f6c83447c
MYMiyoung <al2969@naver.com>
2022-12-29 09:57:52 +0900

    Initial Commit

---
+++ gitTest2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitTest2
+//
+//  Created by 김미영 on 2022/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
